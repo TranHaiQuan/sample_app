5367298c4f124786dc3e772170562dfa2fd4f835
tran hai quan <tranhaiquan.thq@gmail.com>
2017-12-05 14:31:12 +0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
   # start by string
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.email.maximum},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.password.minimum}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BBCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   root "static_pages#home"
   get "/signup", to: "users#new"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  post "/signup", to: "users#create"
+  resources :users
 end
