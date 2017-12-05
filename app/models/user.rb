7fb4eb0af5438d089a13af53655bca812142ae2f
tran hai quan <tranhaiquan.thq@gmail.com>
2017-12-05 14:31:12 +0700

    Make a basic User model (including secure password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {maximum: Settings.user.name.maximum}
+  # start by string
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.user.email.maximum},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.user.password.minimum}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,12 @@
-ActiveRecord::Schema.define(version: 20171206064822) do
+ActiveRecord::Schema.define(version: 20171210232232) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
