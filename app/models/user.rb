20a949d23565c3338540b3e8539b7bb9cc57aaea
tran hai quan <tranhaiquan.thq@gmail.com>
2017-12-12 11:37:37 +0700

    Fix conflic custom

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # start by string
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.email.maximum},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
+   format: {with: VALID_EMAIL_REGEX},
+   uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.password.minimum}
 end
