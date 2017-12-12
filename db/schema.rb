835018e2b5a36f8a11ff6945063c5f8c3639d3f2
tran hai quan <tranhaiquan.thq@gmail.com>
2017-12-12 16:31:20 +0700

    Use SSL and the Puma webserver in production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206064822) do
+ActiveRecord::Schema.define(version: 20171210232232) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
