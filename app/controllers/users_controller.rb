24dd842f662d13053838c2a982916502daa8e1d8
tran hai quan <tranhaiquan.thq@gmail.com>
2017-12-21 16:04:21 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find params[:id]
+    @microposts = @user.microposts.paginate page: params[:page]
   end
 
   def new
@@ -51,14 +52,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = t "controllers.users_controller.please_login"
-      redirect_to login_url
-    end
   end
 
   def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ User.create!(name: "Tran Hai Quan", email: "tranhaiquan@gmail.com",
   User.create!(name: name, email: email, password: password,
     password_confirmation: password_confirmation,  activated: true, activated_at: Time.zone.now)
 end
+users = User.order(:created_at).take 6
+50.times do
+  content = Faker::Lorem.sentence 5
+  users.each {|user| user.microposts.create! content: content}
+end
